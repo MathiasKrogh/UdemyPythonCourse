27e2fdcb0324ff828bebf9e13cc42d5fe413b517
au701034 <202106737@post.au.dk>
2024-04-11 15:45:17 +0200

    Dag 5 færdig

---
+++ day5-tests.py
@@ -0,0 +1,11 @@
+fruits = ["Apple", "Banana", "Peaches"]
+for fruit in fruits:
+    print(fruit + " Pie")
+
+sum = 0
+start = 1
+end = 10
+for number in range(start, end + 1, 2):
+    print(number)
+    sum += number
+print(f"The sum of the numbers from {start} to {end} is {sum}")

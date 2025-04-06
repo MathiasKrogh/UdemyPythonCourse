0c859692c7e400a86e6031cdb24b0de784418bb5
MathiasKrogh <Mathias@bondekrogh.dk>
2025-04-07 00:40:53 +0200

    Refactoring

--- day16/main.py
+++ day16/main.py
@@ -15,10 +15,8 @@ if __name__ == "__main__":
         elif choice == "report":
             coffee_maker.report()
             money_machine.report()
-            continue
-        drink = menu.find_drink(choice)
-        if drink != None:
-            if coffee_maker.is_resource_sufficient(drink):
-                valid_payment = money_machine.make_payment(drink.cost)
-                if valid_payment:
+        else: 
+            drink = menu.find_drink(choice)
+            if drink != None:
+                if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
                     coffee_maker.make_coffee(drink)
